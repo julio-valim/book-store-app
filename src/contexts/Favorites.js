e492f9bd1136e68fd26cbdac3f3ddcc1026e2cd7
julio-valim <juliopvalim@gmail.com>
2023-10-02 14:45:57 -0300

    create context component to manage favorites

---
+++ src/contexts/Favorites.js
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+export const FavoritesContext = createContext();
+FavoritesContext.displayName = "FavoritesBooks"
+
+export default function FavoritesProvider({ children }) {
+
+  const [ favorite, setFavorite ] = useState([])
+
+  return (
+    <FavoritesContext.Provider
+      value={{ favorite, setFavorite }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
+export function useFavoriteContext(){
+  const { favorite, setFavorite } = useContext(FavoritesContext)
+
+  function addFavorite(newFavorite){
+    const repeatedFavorite = favorite.some((el)=>el.id===newFavorite.id)
+
+    let newFavoriteList = [...favorite]
+
+    if(!repeatedFavorite){
+      newFavoriteList.push(newFavorite)
+      return setFavorite(newFavoriteList)
+    }
+
+    newFavoriteList = favorite.filter((el)=>el.id!==newFavorite.id)
+    return setFavorite(newFavoriteList)
+  }
+  return {
+    favorite,
+    addFavorite
+  }
+}
