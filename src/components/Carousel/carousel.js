3de651c11533771e22fa1129eee4b3ddb4bf3f32
julio-valim <juliopvalim@gmail.com>
2023-10-02 14:45:13 -0300

    create card carousel to components

---
+++ src/components/Carousel/carousel.js
@@ -0,0 +1,10 @@
+export const settings = {
+  dots: false,
+  infinity: false,
+  speed: 300,
+  centerMode: true,
+  variableWidth: true,
+  adaptativeHeight: true,
+  slidesToShow: 5,
+  slidesToScrow: 1
+}
