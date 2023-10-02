fde7344334d2b40931d2e6591611d9790de18161
julio-valim <juliopvalim@gmail.com>
2023-10-02 14:42:01 -0300

    create card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,27 @@
+import { Link } from 'react-router-dom';
 import styles from './Card.module.css'
+// import checkedIcon from './fav_checked.png'
+import uncheckedIcon from './fav_unchecked.png'
+import { useFavoriteContext } from '../../contexts/Favorites';
 
-function Card({ url, cover, title }) {
+function Card({ id, url, cover, title }) {
+
+  const { favorite, addFavorite } = useFavoriteContext()
+
   return(
     <section className={styles.card}>
-      <a href={url} 
+      <Link to={url} 
          rel='noreferrer noopener'
          target='_blank'>
-        <img src={cover} alt={title} />
-      </a>
+        <img className={styles.cover} src={cover} alt={title} />
+      </Link>
+      <figure className={styles.icon}>
+        <img 
+          src={uncheckedIcon}
+          alt='favorite'
+          onClick={()=>addFavorite({id})}  
+        />
+      </figure>
     </section>
   );  
 }
