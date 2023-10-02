4d4dc08560ffc51d0c22ec6abc30a39bd1fd43c6
julio-valim <juliopvalim@gmail.com>
2023-10-02 14:47:14 -0300

    add favorite route; apply context;

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,21 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Search from './pages/Search';
 import NotFound from './pages/NotFound';
+import Favorites from './pages/Favorites';
+import FavoritesProvider from './contexts/Favorites';
 
-function AppRoutes(){
-  return(
+function AppRoutes() {
+  return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/search' element={<Search/>}/>
-        <Route path='*' element={<NotFound/>}/>
-      </Routes>
+      <FavoritesProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/favorites' element={<Favorites />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </FavoritesProvider>
     </BrowserRouter>
   );
 }
